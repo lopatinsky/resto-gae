55bf3c86ed9ae4a76b0f8c27ef4d629d940828be
Mikhail Burshteyn <mdburshteyn@gmail.com>
2014-09-18 18:39:34 +0400

    Remove unneeded crons, refactor /api/status

--- api/status.py
+++ api/status.py
@@ -11,12 +11,10 @@ __author__ = 'mihailnikolaev'
 #TODO: check time
 class StatusRequestHandler(BaseHandler):
     """ /api/status """
-    status_list = list()
 
     def post(self):
-        self.status_list = list()
+        status_list = list()
         #parce data
-        # print self.request.get('json')
         logging.info(self.request.get('json'))
         arr = json.loads(self.request.get('json'))
 
@@ -26,28 +24,17 @@ class StatusRequestHandler(BaseHandler):
             order_id = item['order_id']
 
             order_info = iiko.order_info1(order_id, venue_id)
-            org_id = Venue.venue_by_id(venue_id).company_id
-            venues = iiko.get_venues(org_id)
-            print venues
-            address = ""
-            name = ""
-            #get venue
-            for venue in venues:
-                if venue.venue_id == venue_id:
-                    address = venue.address
-                    name = venue.name
-            order = Order()
-            order.order_id = order_id
-            order.set_status(order_info['status'])
-            order.put()
+            venue = Venue.venue_by_id(venue_id)
+            address = venue.address
+            name = venue.name
+
             #make dict
-            self.status_list.append({
+            status_list.append({
                 'order_id': order_id,
                 'venue_id': venue_id,
-                'status': order.status,
+                'status': Order.parse_status(order_info['status']),
                 'address': address,
                 'name': name,
                 'createdTime': iiko.order_info1(order_id, venue_id)['createdTime']
             })
-            order.key.delete()
-        self.render_json({"orders": self.status_list})
+        self.render_json({"orders": status_list})
