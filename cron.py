9c0d9926281c14a4011d4994edb58291d051092c
Mikhail Burshteyn <mdburshteyn@gmail.com>
2014-09-30 11:04:28 +0400

    Revert "Add dirty hack for testing refactored version"
    This reverts commit c0df96c22504be2aeb8b76e51808be760a12223f.

--- cron.py
+++ cron.py
@@ -1,6 +1,5 @@
 # coding=utf-8
 
-from methods import memcache_fix  # TODO remove
 import logging
 
 import webapp2
--- main.py
+++ main.py
@@ -15,7 +15,6 @@
 # limitations under the License.
 #
 import webapp2
-from methods import memcache_fix  # TODO remove
 from api import *
 
 
