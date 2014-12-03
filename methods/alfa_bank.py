31ab9265a601dbe3323629dabb9359d14232286e
Mikhail Burshteyn <mdburshteyn@gmail.com>
2014-12-03 20:54:04 +0300

    Add method for checking extended payment status

--- methods/alfa_bank.py
+++ methods/alfa_bank.py
@@ -5,6 +5,12 @@ from google.appengine.api import urlfetch
 from config import config
 
 
+ALPHA_CARD_LIMIT_CODES = [-20010, 902, 116, 123]
+ALPHA_WRONG_CREDENTIALS_CODES = [71015]
+CARD_LIMIT_CODE = 1
+CARD_WRONG_CREDENTIALS_CODE = 2
+
+
 def __post_request_alfa(api_path, params):
     url = '%s%s' % (config.ALFA_BASE_URL, api_path)
     payload = json.dumps(params)
@@ -37,6 +43,27 @@ def check_status(company, order_id):
     }
     result = __post_request_alfa('/rest/getOrderStatus.do', params)
     return json.loads(result)
+
+
+def check_extended_status(company, order_id):
+    params = {
+        'userName': company.alpha_login,
+        'password': company.alpha_pass,
+        'orderId': order_id,
+    }
+    result = __post_request_alfa('/rest/getOrderStatusExtended.do', params)
+    result_json = json.loads(result)
+    if result_json['actionCode'] in ALPHA_CARD_LIMIT_CODES:
+        status_code = CARD_LIMIT_CODE
+    elif result_json['actionCode'] in ALPHA_WRONG_CREDENTIALS_CODES:
+        status_code = CARD_WRONG_CREDENTIALS_CODE
+    else:
+        status_code = result_json['actionCode']
+    return {
+        'error_code': status_code,
+        'description': result_json['actionCodeDescription'],
+        'alfa_response': result_json
+    }
 
 
 def get_back_blocked_sum(company, order_id):
