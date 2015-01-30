05789be3eb1d91108edc3479c3b7f3ebd6806a60
dvpermyakov <dvpermyakov@edu.hse.ru>
2015-01-30 16:10:28 +0300

    add iiko clients to report

--- mt/report/clients.py
+++ mt/report/clients.py
@@ -4,52 +4,26 @@ from ..base import BaseHandler
 from datetime import datetime
 from models import iiko
 from report_methods import suitable_date, PROJECT_STARTING_YEAR
+from methods import iiko_api
 import logging
 
 
 class ClientsReportHandler(BaseHandler):
 
-    @staticmethod
-    def get_clients_info(chosen_year=0, chosen_month=0, chosen_day=0, venue_id=None):
-        logging.info('venue_id: %s' % venue_id)
-        query = iiko.Order.query(iiko.Order.date >= suitable_date(chosen_day, chosen_month, chosen_year, True))
-        query = query.filter(iiko.Order.date <= suitable_date(chosen_day, chosen_month, chosen_year, False))
-        if venue_id:
-            query = query.filter(iiko.Order.venue_id == venue_id)
+    def get_iiko_clients_info(self, clients, start, end):
 
-        clients = {}
-        for order in query.fetch():
-            if not order.customer:
-                continue
-            client = order.customer.get()
-            if client.key.id() not in clients:
-                if order.status == iiko.Order.CLOSED:
-                    client.order_number = 1
-                    client.total_sum = order.sum
-                    client.cancelled = 0
-                    client.cancelled_sum = 0
-                    if order.payment_type == iiko.Order.CARD:
-                        client.card = 1
-                    else:
-                        client.card = 0
-                elif order.status == iiko.Order.CANCELED:
-                    client.order_number = 0
-                    client.total_sum = 0
-                    client.cancelled = 1
-                    client.cancelled_sum = order.sum
-                    client.card = 0
-                if order.status == iiko.Order.CLOSED or order.status == iiko.Order.CANCELED:
-                    clients[client.key.id()] = client
-            else:
-                client = clients[client.key.id()]
-                if order.status == iiko.Order.CLOSED:
-                    client.order_number += 1
-                    client.total_sum += order.sum
-                    if order.payment_type == iiko.Order.CARD:
-                        client.card = 1
-                elif order.status == iiko.Order.CANCELED:
-                    client.cancelled += 1
-                    client.cancelled_sum += order.sum
+        for client in clients.values():
+            orders = iiko_api.get_history(client.customer_id, client.venue_id)
+            orders = orders['historyOrders']
+            for order in orders:
+                logging.info(order)
+                if order.get('status'):
+                    if iiko.Order.parse_status(order['status']) == iiko.Order.CLOSED:
+                        client.order_number += 1
+                        client.total_sum += order['sum']
+                    elif iiko.Order.parse_status(order['status']) == iiko.Order.CANCELED:
+                        client.cancelled += 1
+                        client.cancelled_sum += order['sum']
         clients = clients.values()
         total = {
             'order_number': sum(client.order_number for client in clients),
@@ -58,10 +32,61 @@ class ClientsReportHandler(BaseHandler):
             'cancelled': sum(client.cancelled for client in clients),
             'cancelled_sum': sum(client.cancelled_sum for client in clients)
         }
-
         return clients, total
 
+    def get_app_clients_info(self, orders, clients):
+
+        for order in orders:
+            client = clients[order.client_id]
+            if order.status == iiko.Order.CLOSED:
+                client.order_number += 1
+                client.total_sum += order.sum
+                if order.payment_type == iiko.Order.CARD:
+                    client.card += 1
+            elif order.status == iiko.Order.CANCELED:
+                client.cancelled += 1
+                client.cancelled_sum += order.sum
+        clients = clients.values()
+        total = {
+            'order_number': sum(client.order_number for client in clients),
+            'card_order': sum(client.card for client in clients),
+            'total_sum': sum(client.total_sum for client in clients),
+            'cancelled': sum(client.cancelled for client in clients),
+            'cancelled_sum': sum(client.cancelled_sum for client in clients)
+        }
+        return clients, total
+
+    def get_clients_info(self, chosen_year=0, chosen_month=0, chosen_day=0, venue_id=None, chosen_type='app'):
+        start = suitable_date(chosen_day, chosen_month, chosen_year, True)
+        end = suitable_date(chosen_day, chosen_month, chosen_year, False)
+        query = iiko.Order.query(iiko.Order.date >= start, iiko.Order.date <= end)
+        if venue_id:
+            query = query.filter(iiko.Order.venue_id == venue_id)
+        orders = query.fetch()
+
+        clients = {}
+        for order in orders[:]:
+            if not order.customer:
+                orders.remove(order)
+                continue
+            client = order.customer.get()
+            order.client_id = client.customer_id
+            if client.customer_id not in clients:
+                client.order_number = 0
+                client.total_sum = 0
+                client.cancelled = 0
+                client.cancelled_sum = 0
+                client.card = 0
+                client.venue_id = order.venue_id
+                clients[client.customer_id] = client
+
+        if chosen_type == 'app':
+            return self.get_app_clients_info(orders, clients)
+        else:
+            return self.get_iiko_clients_info(clients, start, end)
+
     def get(self):
+        chosen_type = self.request.get("selected_type")
         venue_id = self.request.get("selected_venue")
         chosen_year = self.request.get_range("selected_year")
         chosen_month = self.request.get_range("selected_month")
@@ -72,12 +97,13 @@ class ClientsReportHandler(BaseHandler):
             chosen_day = 0
         if not venue_id:
             venue_id = 0
+            chosen_type = 'app'
             chosen_year = datetime.now().year
             chosen_month = datetime.now().month
             chosen_day = datetime.now().day
         if venue_id == '0':
             venue_id = None
-        clients, total = self.get_clients_info(chosen_year, chosen_month, chosen_day, venue_id)
+        clients, total = self.get_clients_info(chosen_year, chosen_month, chosen_day, venue_id, chosen_type)
         values = {
             'venues': iiko.Venue.query().fetch(),
             'clients': clients,
@@ -87,6 +113,7 @@ class ClientsReportHandler(BaseHandler):
             'end_year': datetime.now().year,
             'chosen_year': chosen_year,
             'chosen_month': chosen_month,
-            'chosen_day': chosen_day
+            'chosen_day': chosen_day,
+            'chosen_type': chosen_type
         }
         self.render('reported_clients.html', **values)
