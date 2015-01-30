0c0e175b77fe21671c8caca7df948c99d6b9e1c1
Mikhail Burshteyn <mdburshteyn@gmail.com>
2015-01-30 14:40:08 +0300

    Add task to update Coffee&City order and cron to start task

--- cron/__init__.py
+++ cron/__init__.py
@@ -1,12 +1,16 @@
 # coding=utf-8
 
 import webapp2
-from cron.update_menu import UpdateMenuHandler
-from cron.update_orders import UpdateOrdersHandler
-from cron.mivako_gifts import MivakoGiftsEmailHandler
+from .update_menu import UpdateMenuHandler
+from .update_orders import UpdateOrdersHandler
+from .mivako_gifts import MivakoGiftsEmailHandler
+from .coffee_city_updates import CoffeeCityUpdatesHandler, CheckCoffeeCityUpdatesHandler
 
 app = webapp2.WSGIApplication([
     ('/cron/update_orders', UpdateOrdersHandler),
     ('/cron/update_menu', UpdateMenuHandler),
     ('/cron/mivako_gifts', MivakoGiftsEmailHandler),
+
+    ('/cron/check_coffee_city', CheckCoffeeCityUpdatesHandler),
+    ('/task/update_coffee_city', CoffeeCityUpdatesHandler),
 ], debug=True)
