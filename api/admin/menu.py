c1bfcd1d6d324a4265f1a6f82e0c0fbcaf9fac93
Mikhail Burshteyn <mdburshteyn@gmail.com>
2015-04-27 15:57:54 +0300

    Change admin menu format

--- api/admin/menu.py
+++ api/admin/menu.py
@@ -1,3 +1,6 @@
+# coding=utf-8
+
+from methods import iiko_api
 from models.admin import Admin
 from models.iiko import DeliveryTerminal
 
@@ -6,13 +9,35 @@ __author__ = 'dvpermyakov'
 from ..base import BaseHandler
 
 
+def _process_category(category):
+    return {
+        "info": {
+            "category_id": category["id"],
+            "title": category["name"]
+        },
+        "items": [_convert_product(p) for p in category["products"]],
+        "children": [_process_category(c) for c in category["children"]]
+    }
+
+
+def _convert_product(product):
+    return {
+        "id": product["productId"],
+        "title": product["name"]
+    }
+
+
 class MenuHandler(BaseHandler):
     def get(self):
         token = self.request.get("token")
         admin = Admin.query(Admin.token == token).get()
         if not admin:
             self.abort(401)
-        self.redirect('/api/venue/%s/menu?all' % admin.delivery_terminal_id)
+        menu = iiko_api.get_menu(admin.company_id)
+        processed_menu = [_process_category(c) for c in menu]
+        self.render_json({
+            "menu": processed_menu
+        })
 
 
 class DynamicInfoHandler(BaseHandler):
