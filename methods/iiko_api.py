7fc4d5d2d9739625b04632f73ba35c1ca57858ee
Mikhail Burshteyn <mdburshteyn@gmail.com>
2015-06-24 01:34:05 +0300

    Use platius for customer endpoints

--- methods/iiko_api.py
+++ methods/iiko_api.py
@@ -731,7 +731,7 @@ def get_customer_by_phone(company, phone):
     result = __get_request(company, '/customers/get_customer_by_phone', {
         'organization': company.iiko_org_id,
         'phone': phone
-    })
+    }, force_iiko_net=True)
     return json.loads(result)
 
 
@@ -739,14 +739,14 @@ def get_customer_by_id(company, customer_id):
     result = __get_request(company, '/customers/get_customer_by_id', {
         'organization': company.iiko_org_id,
         'id': customer_id
-    })
+    }, force_iiko_net=True)
     return json.loads(result)
 
 
 def create_or_update_customer(company, data):
     result = __post_request(company, '/customers/create_or_update', {
         'organization': company.iiko_org_id
-    }, {'customer': data})
+    }, {'customer': data}, force_iiko_net=True)
     return result.strip('"')
 
 
