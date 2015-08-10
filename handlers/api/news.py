b9ea3221999ba28d6f108b9aa7d82a43dd245092
dvpermyakov <dvpermyakov1@gmail.com>
2015-08-10 15:50:03 +0300

    move get news method to news model

--- handlers/api/news.py
+++ handlers/api/news.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from handlers.api.base import BaseHandler
 from models.iiko import CompanyNew
+from models.specials import News
 
 __author__ = 'dvpermyakov'
 
@@ -8,7 +9,7 @@ __author__ = 'dvpermyakov'
 class NewsHandler(BaseHandler):
     def get(self, company_id):
         company = CompanyNew.get_by_id(int(company_id))
-        news = company.get_news()
+        news = News.get(company)
         self.render_json({
             "news": news.dict() if news else None,
             "card_button_text": company.card_button_text or u"Добавить карту",
