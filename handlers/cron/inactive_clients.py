62dfdac1aa04a2b03fe245452cd76bd78b44215e
dvpermyakov <dvpermyakov1@gmail.com>
2015-08-13 21:50:01 +0300

    update inactive clients with promos cron

--- handlers/cron/inactive_clients.py
+++ handlers/cron/inactive_clients.py
@@ -8,10 +8,16 @@ from models.iiko import Order
 __author__ = 'dvpermyakov'
 
 
+COMPANIES = ['5cae16f4-4039-11e5-80d2-d8d38565926f']
+DAYS = 7
+
+
 class InactiveClientsWithPromo(RequestHandler):
     def get(self):
-        start = datetime.utcnow() - timedelta(days=7)
-        orders = Order.query(Order.date <= start).fetch()
+        start = datetime.utcnow() - timedelta(days=DAYS+1)
+        end = datetime.utcnow() - timedelta(days=DAYS)
+        orders = Order.query(Order.date >= start, Order.date <= end).fetch()
         text = u'7 дней после заказа'
         for order in orders:
-            deferred.defer(send_order_screen_push, order, text)
+            if order.venue_id in COMPANIES:
+                deferred.defer(send_order_screen_push, order, text)
