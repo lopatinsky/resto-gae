91950458d69eca418c31c94fc7c87f863f0d503d
dvpermyakov <dvpermyakov1@gmail.com>
2015-08-28 20:01:07 +0300

    fix method of request for changing order in auto

--- methods/auto/request.py
+++ methods/auto/request.py
@@ -15,18 +15,18 @@ def cancel_oder(order, auto_venue):
     }
     url = '%s%s?%s' % (BASE_URL, path, urllib.urlencode(params))
     logging.info(url)
-    response = urlfetch.fetch(url)
+    response = urlfetch.fetch(url, method='POST')
     logging.info(response.status_code)
 
 
 def close_order(order, auto_venue):
-    path = '/orders/%s/cancel' % order.key.id()
+    path = '/orders/%s/close' % order.key.id()
     params = {
         'token': auto_venue.token
     }
     url = '%s%s?%s' % (BASE_URL, path, urllib.urlencode(params))
     logging.info(url)
-    response = urlfetch.fetch(url)
+    response = urlfetch.fetch(url, method='POST')
     logging.info(response.status_code)
 
 
@@ -37,5 +37,5 @@ def confirm_order(order, auto_venue):
     }
     url = '%s%s?%s' % (BASE_URL, path, urllib.urlencode(params))
     logging.info(url)
-    response = urlfetch.fetch(url)
+    response = urlfetch.fetch(url, method='POST')
     logging.info(response.status_code)
