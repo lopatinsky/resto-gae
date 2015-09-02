8a3fd69b8c080ad898c4d42998b47005a39f1bf6
Mikhail Burshteyn <mdburshteyn@gmail.com>
2015-09-02 13:37:53 +0300

    Don't return money when order is canceled in OE

--- methods/orders/cancel.py
+++ methods/orders/cancel.py
@@ -1,6 +1,9 @@
+# coding=utf-8
+
 import logging
 from methods.alfa_bank import get_back_blocked_sum
 from methods.auto.request import cancel_oder
+from methods.email import mandrill
 from methods.parse_com import send_order_status_push
 from models.auto import AutoVenue
 from models.iiko import CompanyNew, DeliveryTerminal, PaymentType
@@ -18,10 +21,17 @@ def cancel(order):
     if auto_venue:
         cancel_oder(order, auto_venue)
     if order.payment_type == PaymentType.CARD:
-        cancel_result = get_back_blocked_sum(company, order.alfa_order_id)
-        logging.info("cancel %s" % str(cancel_result))
-        success = 'errorCode' not in cancel_result or str(cancel_result['errorCode']) == '0'
-        if not success:
-            logging.warning("cancel failed")
-            return
+        if order.venue_id == CompanyNew.ORANGE_EXPRESS:
+            mandrill.send_email("noreply-order@ru-beacon.ru",
+                                ["kapus78@mail.ru"],
+                                ["mdburshteyn@gmail.com", "isparfenov@gmail.com"],
+                                u"[ОЭ] Отмена заказа с безналичной оплатой",
+                                u"№ заказа: %s, сумма: %s, платеж: %s" % (order.number, order.sum, order.alfa_order_id))
+        else:
+            cancel_result = get_back_blocked_sum(company, order.alfa_order_id)
+            logging.info("cancel %s" % str(cancel_result))
+            success = 'errorCode' not in cancel_result or str(cancel_result['errorCode']) == '0'
+            if not success:
+                logging.warning("cancel failed")
+                return
     send_order_status_push(order)
