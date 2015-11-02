28c235dd7572f890ba5d650e138960bae7351342
Mikhail Burshteyn <mdburshteyn@gmail.com>
2015-11-02 18:57:21 +0300

    Change Mivako gift roll

--- handlers/api/specials/mivako_promo.py
+++ handlers/api/specials/mivako_promo.py
@@ -8,7 +8,7 @@ class MivakoPromoInfoHandler(BaseHandler):
     def get(self):
         promo_info = {
             "title": u"Подари ролл другу!",
-            "text": u"Укажи номер телефона друга и мы подарим ему ролл \"Филадельфия Люкс\" при первом заказе.",
+            "text": u"Укажи номер телефона друга и мы подарим ему ролл \"Калифорния с лососем\" при первом заказе.",
             "image": "http://lh6.ggpht.com/"
                      "8zevGQVTtrVFeEE2CNfHX3fXjV4BiPCd_EtlIdAs0Zq_OZ0sU9wrz9w2TOsbWUkVChtviitwCdFfk5PObmLW0WXqc9A"
         }
@@ -26,13 +26,13 @@ class MivakoPromoSendGiftHandler(BaseHandler):
         recipients = zip(to, names)
         for phone, name in recipients:
             MivakoGift(sender=from_, recipient=phone, recipient_name=name,
-                       gift_item=u"ролл \"Филадельфия Люкс\"").put()
+                       gift_item=u"ролл \"Калифорния с лососем\"").put()
 
         logging.info("from: %s", from_)
         logging.info("to: %r", recipients)
 
         self.render_json({
-            "sms": u"""Привет! Дарю тебе ролл "Филадельфия Люкс"!
+            "sms": u"""Привет! Дарю тебе ролл "Калифорния с лососем"!
 Получи его при заказе в ресторане www.mivako.ru
 Скачай приложение: https://clck.ru/9NFUj"""
         })
