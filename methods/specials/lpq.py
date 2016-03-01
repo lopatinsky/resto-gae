9830f42b08bebde252ffe9f31e4edddfc20488f0
Mikhail Burshteyn <mdburshteyn@gmail.com>
2016-03-01 12:37:46 +0300

    Use iiko categories in evening discounts

--- methods/specials/lpq.py
+++ methods/specials/lpq.py
@@ -5,16 +5,15 @@ from methods import working_hours
 from models.iiko.company import CompanyNew
 
 _TEN_PERCENT_ALWAYS_CATEGORIES = [
-    "b934ff20-8f86-46d2-b438-090b8fc3c0cf",
-    "2b29605c-f184-4409-a56b-c444db587a38",
+    u"Кофе на вынос",
 ]
 
 _EVENING_CATEGORIES = [
-    "9d1a068a-fdd5-46e8-bfbf-17f7512fc648",
-    "a7b9dcc0-9650-42dc-bdd1-4cdcd650b879",
-    "6a8d7a61-88f6-42eb-9d95-99cf71060d38",
+    u"Хлеб",
+    u"Граб-н-Гоу",
+    u"Выпечка",
+    u"Десерты (24 часа)",
 ]
-_EVENING_PRODUCT_CODES = ["501188", "501187", "103085"]
 _EVENING_SCHEDULE = {
     "dc12db4e-f192-c4d2-0152-d2c383885e4f": {  # Lesnaya 5
         "days": [1, 2, 3, 4, 5, 6, 7],
@@ -41,9 +40,9 @@ def _add_discount(order, item, discount_fraction):
 def apply_lpq_discounts(order):
     company = CompanyNew.get_by_iiko_id(order.venue_id)
     for item in order.items:
-        if item['ext_category_id'] in _TEN_PERCENT_ALWAYS_CATEGORIES:
+        if item['category'] in _TEN_PERCENT_ALWAYS_CATEGORIES:
             _add_discount(order, item, 0.1)
-        elif item['ext_category_id'] in _EVENING_CATEGORIES or item['code'] in _EVENING_PRODUCT_CODES:
+        elif item['category'] in _EVENING_CATEGORIES:
             if order.delivery_terminal_id in _EVENING_SCHEDULE:
                 schedule = [_EVENING_SCHEDULE[order.delivery_terminal_id]]
                 local_date = order.date + timedelta(seconds=company.get_timezone_offset())
