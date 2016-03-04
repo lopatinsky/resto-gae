b3f8c6ed618d49e3826197a6b5cef44634de812e
Mikhail Burshteyn <mdburshteyn@gmail.com>
2016-03-04 14:43:25 +0300

    Configure discounts

--- methods/specials/lpq.py
+++ methods/specials/lpq.py
@@ -7,6 +7,9 @@ from models.iiko.company import CompanyNew
 _TEN_PERCENT_ALWAYS_CATEGORIES = [
     u"Кофе на вынос",
 ]
+_TEN_PERCENT_EXCLUDED_VENUES = {
+    "f1516ef4-6224-a808-0153-3e5b7ba2af5b",
+}
 
 _EVENING_CATEGORIES = [
     u"Хлеб",
@@ -26,7 +29,19 @@ _EVENING_SCHEDULE = {
     "ce1b7218-7925-b427-0152-e0cb68223988": {  # Kuznetsky most
         "days": [1, 2, 3, 4, 5, 6, 7],
         "hours": "21-23"
-    }
+    },
+    "1bd13391-4c56-29b6-0152-ffeb8a0a30a2": {  # Paveletskaya
+        "days": [1, 2, 3, 4, 5, 6, 7],
+        "hours": "21-23"
+    },
+    "f1516ef4-6224-a808-0153-3e5b7ba2af5b": {  # Park
+        "days": [1, 2, 3, 4, 5, 6, 7],
+        "hours": "21-23"
+    },
+    "f1516ef4-6224-a808-0153-3e5b7ba2ab0e": {  # Michurinskiy
+        "days": [1, 2, 3, 4, 5, 6, 7],
+        "hours": "19-24"
+    },
 }
 
 
@@ -40,7 +55,8 @@ def _add_discount(order, item, discount_fraction):
 def apply_lpq_discounts(order):
     company = CompanyNew.get_by_iiko_id(order.venue_id)
     for item in order.items:
-        if item['category'] in _TEN_PERCENT_ALWAYS_CATEGORIES:
+        if item['category'] in _TEN_PERCENT_ALWAYS_CATEGORIES \
+                and order.delivery_terminal_id not in _TEN_PERCENT_EXCLUDED_VENUES:
             _add_discount(order, item, 0.1)
         elif item['category'] in _EVENING_CATEGORIES:
             if order.delivery_terminal_id in _EVENING_SCHEDULE:
